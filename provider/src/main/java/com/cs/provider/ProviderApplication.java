8ac869b53cc920852c5a55d13c1e60b351a9e069
chenss <1979520767@qq.com>
2019-12-28 12:48:55 +0800

    hh

--- provider/src/main/java/com/cs/provider/ProviderApplication.java
+++ provider/src/main/java/com/cs/provider/ProviderApplication.java
@@ -1,13 +1,13 @@
-package com.cs.common.server;
+package com.cs.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 
 }
--- provider/src/test/java/com/cs/provider/ProviderApplicationTests.java
+++ provider/src/test/java/com/cs/provider/ProviderApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cs.common.server;
+package com.cs.provider;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class ProviderApplicationTests {
 
     @Test
     void contextLoads() {
